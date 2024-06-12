22182905e77249bb5c9531bed757181f874c9449
frederikqfunk <101515868+frederikqfunk@users.noreply.github.com>
2024-06-12 17:37:19 +0200

    Update status_check.cpp

--- cgi-bin/status_check.cpp
+++ cgi-bin/status_check.cpp
@@ -2,15 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <regex>
 using namespace std;
+
+bool isValidInput(const char *value) {
+    // Tillad kun alfanumeriske tegn og visse andre sikre tegn
+    regex pattern("^[a-zA-Z0-9-_]+$");
+    return regex_match(value, pattern);
+}
 
 int main () {
    int status;
    char t[1024]="ps -eo lstart,cmd | grep ";
    cout << "Content-type:text/html\r\n\r\n"<<endl;
    char *value = getenv("QUERY_STRING");
-   strcat(t,value);
-   status = system(strcat(t," | grep -v grep | head -n 1 | awk '{ print $1\" \"$3\" \"$2\" \"$5\" \"$4}'"));
+
+   // Valider input
+   if (value != NULL && isValidInput(value)) {
+       strcat(t,value);
+       status = system(strcat(t," | grep -v grep | head -n 1 | awk '{ print $1\" \"$3\" \"$2\" \"$5\" \"$4}'"));
+   } else {
+       cout << "Invalid input" << endl;
+   }
    
    return 0;
 }
